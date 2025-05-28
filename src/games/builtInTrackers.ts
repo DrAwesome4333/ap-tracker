2097ea991cf518ffa42cc781aa39daaec32fb0c1
Dr.Awesome4333 <22409322+DrAwesome4333@users.noreply.github.com>
2025-05-28 16:16:37 -0600

    Remove demo tracker from tracker list

--- src/games/builtInTrackers.ts
+++ src/games/builtInTrackers.ts
@@ -1,6 +1,8 @@
-import tracker_oot from "./Ocarina of Time/tracker";
+// import tracker_oot from "./Ocarina of Time/tracker";
 import { Tracker } from "./TrackerManager";
 
-const builtInTrackers: Tracker[] = [tracker_oot];
+const builtInTrackers: Tracker[] = [
+    // tracker_oot
+];
 
 export { builtInTrackers };
