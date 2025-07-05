78e14aa57347f48358040869aeecfcc2c607efaf
Dr.Awesome4333 <22409322+DrAwesome4333@users.noreply.github.com>
2025-07-05 00:24:54 -0600

    Removed OOT again, will remake example later

--- src/games/builtInTrackers.ts
+++
@@ -1,8 +0,0 @@
-// import tracker_oot from "./Ocarina of Time/tracker";
-import { Tracker } from "../services/tracker/TrackerManager";
-
-const builtInTrackers: Tracker[] = [
-    // tracker_oot
-];
-
-export { builtInTrackers };
