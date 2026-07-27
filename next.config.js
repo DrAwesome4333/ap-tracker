0c591f57d19958954eb97f21a3d5418452ef6e65
DrAwesome4333 <22409322+DrAwesome4333@users.noreply.github.com>
2026-07-26 22:40:36 -0600

    Disable lightning CSS

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     output: "export", // Outputs a Single-Page Application (SPA)
     distDir: "build", // Changes the build output directory to `build`
     assetPrefix: "./", //comment out for local server builds to work with hot reload
+    experimental: {
+        lightningCssFeatures: false, // broke all colors...
+    },
 };
 
 export default (phase) => {
