e6bfac8f94db78f0c150dca1ba7ba48f2971ef9c
DrAwesome4333 <22409322+DrAwesome4333@users.noreply.github.com>
2026-07-26 22:53:30 -0600

    Disable specific features as lightningCss is forced on...

--- next.config.js
+++ next.config.js
@@ -7,7 +7,9 @@ const nextConfig = {
     distDir: "build", // Changes the build output directory to `build`
     assetPrefix: "./", //comment out for local server builds to work with hot reload
     experimental: {
-        useLightningcss: false,
+        lightningCssFeatures: {
+            exclude: ["oklab-colors", "light-dark"],
+        },
     },
 };
 
